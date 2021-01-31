07516153286353bdc2fa90cb39e312d81df88140
j-blalock <justin.m.blalock@gmail.com>
2021-01-30 19:58:11 -0600

    for loop practice

--- experiments.js
+++ experiments.js
@@ -1,4 +1,21 @@
-const monthlyRent = 500;
+// const monthlyRent = 500;
 
-const yearlyRent = monthlyRent * 12;
-console.log(yearlyRent);	
+// const yearlyRent = monthlyRent * 12;
+// console.log(yearlyRent);	
+
+/*
+  Write some code that declares two variables, character and timesToRepeact.
+  Using a loop, repeat that character that many times and then console.log it.
+  Example, if I had character = 'f' and timesToRepeat = 5, it'd console.log
+  'fffff'
+*/
+
+let character = 'a';
+let ans = '';
+const timesToRepeat = 50;
+
+for (let i = 0; i < timesToRepeat; i++) {
+	ans += character;
+}
+
+console.log(ans);
